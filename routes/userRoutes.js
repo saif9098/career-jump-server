f9a606a65f4f342eb66e74aa4b425916bc2a8ad2
Mo. Saif raja <126969644+saif9098@users.noreply.github.com>
2025-04-06 19:42:59 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,30 @@
 import express from "express";
 import {
+  getUsersController,
   updateUserController,
 } from "../controllers/userController.js";
-import {userAuth,isAdmin} from "../middelwares/authMiddleware.js";
+import {userAuth,isAdmin,isAuthor, isUser} from "../middelwares/authMiddleware.js";
 
 //router object
 const router = express.Router();
 
 //routes
 // GET USER DATA || POST
-router.get("/getUser", userAuth, (req, res) => {
+router.get("/getAuth", userAuth, (req, res) => {
+  res.status(200,"hello").send({ ok: true });
+});
+router.get("/getUser", userAuth,isUser, (req, res) => {
   res.status(200,"hello").send({ ok: true });
 });
 //protected Admin route auth
 router.get("/admin-auth", userAuth, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
+//protected Author route auth
+router.get("/author-auth", userAuth, isAuthor, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+router.get("/users/:info",  getUsersController);
 
 // UPDATE USER || PUT
 router.put("/update-user", userAuth, updateUserController);
